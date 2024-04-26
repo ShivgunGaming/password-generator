0d821b5ebf266eb46e77ca7f9c3dbf6e79cb3472
$ʜɪᴠɢᴜɴ ᴛʜᴇ ᴅᴇᴠ ᴅᴇᴍᴏɴ <102505925+ShivgunGaming@users.noreply.github.com>
2024-04-26 13:01:34 +1000

    Create password-generator.py

---
+++ password-generator.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def generate_password(length=12):
+    """Generate a random password."""
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    password_length = int(input("Enter the length of the password: "))
+    password = generate_password(password_length)
+    print("Generated Password:", password)
